45601707569bb5eff58afcc337a450d252cf1f43
David Wesst <david@cocobokostudios.com>
2025-08-24 17:29:20 +0000

    feature. Hiding site from search engines.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src',
+  publicDir: '../public',
   build: {
     outDir: '../dist', // Output to project root's dist directory
     emptyOutDir: true, // Optional: cleans the output dir before building
