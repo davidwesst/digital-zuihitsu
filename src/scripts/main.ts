54690391abad1d69be745236dcbc956eae1e68ff
David Wesst <david@cocobokostudios.com>
2025-08-25 05:48:33 +0000

    chore. Project structure cleanup.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,9 @@
 import '../styles/style.css'
-import { initializeLocalTime } from './localTimer'
 
-initializeLocalTime();
+import { initializeLocalTime } from './widgets/localTime'
+
+document.addEventListener('DOMContentLoaded', () => {
+    initializeLocalTime();
+})
 
 console.log('Well, aren\'t you a curious one? 😉')
