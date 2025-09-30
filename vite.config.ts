2bb3afdffea7de9db8402a754ad44154ffe22d0f
David Wesst <david@cocobokostudios.com>
2025-09-30 01:22:38 -0500

    fix. Added other pages to the build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
   root: 'src',
@@ -6,5 +7,12 @@ export default defineConfig({
   build: {
     outDir: '../dist', // Output to project root's dist directory
     emptyOutDir: true, // Optional: cleans the output dir before building
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./src/index.html', import.meta.url)),
+        blog: fileURLToPath(new URL('./src/blog.html', import.meta.url)),
+        gamelog: fileURLToPath(new URL('./src/blog/gamelog.html', import.meta.url)),
+      },
+    },
   }
 })
