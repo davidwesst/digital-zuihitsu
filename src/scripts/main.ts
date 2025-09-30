7ea12b35177c07c1c17843a59ec3f5d2b7cc87d3
David Wesst <david@cocobokostudios.com>
2025-09-30 00:43:32 -0500

    feature. Added blog to nav.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,3 +4,7 @@ import './components/siteNav/siteNav'
 import './components/localTime/localTime'
 
 console.log('Well, aren\'t you a curious one? 😉')
+
+window.addEventListener('load', () => {
+  document.body?.setAttribute('data-page-ready', 'true')
+})
